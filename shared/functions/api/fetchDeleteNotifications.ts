fc8dc8205b2b0bd334e3b3084c9e75345979281f
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-08-17 15:21:41 +0200

    cleanup console.logs

--- shared/functions/api/fetchDeleteNotifications.ts
+++ shared/functions/api/fetchDeleteNotifications.ts
@@ -2,7 +2,6 @@ import {Endpoints} from "../../Endpoints";
 import type {User} from "../../types";
 
 export async function fetchDeleteNotifications(userId: User['_id']){
-    console.log(userId)
     await fetch(Endpoints.notifications(''), {
         method: 'DELETE',
         body: JSON.stringify({userId}),
