8988b06057e732b4acb70e24eaf4d20a0e339b5d
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-08-17 16:01:26 +0200

    add demo user credentials

--- frontend/src/routes/SignIn.route.tsx
+++ frontend/src/routes/SignIn.route.tsx
@@ -1,9 +1,15 @@
+import DemoUserCredentials from "@/components/auth/DemoUserCredentials.tsx";
 import { SignIn } from "@clerk/clerk-react";
+
+const showDemoUserCredentials = import.meta.env.VITE_SHOW_DEMO_USER_CREDENTIALS;
 
 export default function SignInRoute() {
 	return (
 		<div className="grid place-items-center h-screen w-full bg-primary">
-			<SignIn signUpUrl="/sign-up" withSignUp={false} />
+			<div className="flex flex-col gap-4">
+				<SignIn signUpUrl="/sign-up" withSignUp={false} />
+				{showDemoUserCredentials && <DemoUserCredentials />}
+			</div>
 		</div>
 	);
 }
